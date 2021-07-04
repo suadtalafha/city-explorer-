e98eeb3c8b3261c9cf214c3394b86e78a0c99dbd
suadtalafha <suadhusam@yahoo.com>
2021-07-04 20:05:39 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      cityData:{},
+      search:'',
+      showMap:false,
+
+    }
+  }
+  getLocation = async (e) => {
+    e.preventDefault();
+
+    await this.setState({
+     search: e.target.city.value
+    })
+    let url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.search}&format=json`;
+
+    let respData = await axios.get(url);
+    this.setState({
+      cityData:respData.data[0],
+      showMap:true
+    })
+    
+  }
+  render() {
+    return (
+      <div>
+     <h1>City Explorer</h1>
+     <form onSubmit={this.getLocation}>
+     <input type='text' placeholder='city name' name='city'/>
+          <input type='submit' value='Explore'/>
+
+     </form>
+     <p>City Name: {this.state.cityData.display_name},{this.state.cityData.lat},{this.state.cityData.lon}</p>
+     {this.state.showMap && 
+        <img alt='' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`} />
+        }   
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
